ef6e8947120893c00f6879a70b5ed364dbc7a545
王泓宇 <645702818@qq.com>
2022-08-28 19:53:10 +0800

    实现了使用线程池更新阅读次数

--- blog-api/src/main/java/com/blog/service/ThreadService.java
+++ blog-api/src/main/java/com/blog/service/ThreadService.java
@@ -1,5 +1,8 @@
 package com.blog.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.extension.conditions.update.LambdaUpdateChainWrapper;
 import com.blog.entity.Article;
 import com.blog.mapper.ArticleMapper;
 import org.springframework.scheduling.annotation.Async;
@@ -12,8 +15,15 @@ public class ThreadService {
     @Async("taskExecutor")
     public void updateArticleViewCount(ArticleMapper articleMapper , Article article){
 
+        int viewCounts = article.getViewCounts();
+        Article articleUpdate = new Article();
+        articleUpdate.setViewCounts(viewCounts +1);
+        LambdaUpdateWrapper<Article> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper.eq(Article::getViewCounts,viewCounts);
+        //update article set view_count = 100 where view_count = 99 and id = 11
+        articleMapper.update(articleUpdate, updateWrapper);
         try{
-            Thread.sleep(5000);
+            Thread.sleep(1000);
             System.out.println("更新完成...");
         } catch (InterruptedException e) {
             e.printStackTrace();
